ba57ce1bdace95dfd2f343cbc0b4f3cca6708123
jaem1n207 <tech.jmtt@gmail.com>
2024-05-27 01:12:57 +0900

    chore: healthy code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Bendd",
-  description: "Frontend Software Enginner · jaem1n207",
+  title: 'Bendd',
+  description: 'Frontend Software Enginner · jaem1n207',
   openGraph: {
-    type: "website",
-    url: "https://bendd.me",
+    type: 'website',
+    url: 'https://bendd.me',
   },
 };
 
