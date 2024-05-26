f3fa460a4f723cee6efbea9a250857b477705938
jaem1n207 <tech.jmtt@gmail.com>
2024-05-26 19:44:35 +0900

    feat: shadcn-ui 구성

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bd-flex bd-min-h-screen bd-flex-col bd-items-center bd-justify-between bd-p-24">
       <h1>Bendd</h1>
       <p>Frontend Software Engineer · jaem1n207</p>
+      <Button>Hello!</Button>
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
-        className="dark:invert"
+        className="dark:bd-invert"
         width={100}
         height={24}
         priority
