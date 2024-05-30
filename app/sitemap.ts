894ce94e0da8232875b40db20c7c84cc52676274
jaem1n207 <tech.jmtt@gmail.com>
2024-05-30 23:06:26 +0900

    chore: sitemap url 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export const host = 'https://bendd.me';
-const pathnames = ['/', '/projects', '/articles'];
+const pathnames = ['/', '/article'];
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return pathnames.map(pathname => ({
