bb0e6e3388f30fba48f642d1c61518de15a6c78f
jaem1n207 <tech.jmtt@gmail.com>
2024-06-10 09:53:07 +0900

    style: format with prettier

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,49 +1,49 @@
-import { ImageResponse } from "next/og";
-import type { NextRequest } from "next/server";
-import type { ReactElement } from "react";
+import { ImageResponse } from 'next/og';
+import type { NextRequest } from 'next/server';
+import type { ReactElement } from 'react';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 const interSemiBold = fetch(
-  new URL("./Inter-SemiBold.ttf", import.meta.url),
-).then((res) => res.arrayBuffer());
+  new URL('./Inter-SemiBold.ttf', import.meta.url)
+).then(res => res.arrayBuffer());
 
 export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
   try {
     const { searchParams } = new URL(req.url);
-    const isLight = req.headers.get("Sec-CH-Prefers-Color-Scheme") === "light";
+    const isLight = req.headers.get('Sec-CH-Prefers-Color-Scheme') === 'light';
 
-    const title = searchParams.has("title")
-      ? searchParams.get("title")
-      : "Bendd";
+    const title = searchParams.has('title')
+      ? searchParams.get('title')
+      : 'Bendd';
 
     return new ImageResponse(
       (
         <div
           style={{
-            position: "relative",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            position: 'relative',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
           {isLight ? <LightSvg /> : <DarkSvg />}
           <div
             style={{
-              position: "absolute",
-              fontFamily: "Inter",
-              fontSize: "48px",
-              fontWeight: "600",
-              letterSpacing: "-0.04em",
-              color: isLight ? "black" : "white",
-              top: "250px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              whiteSpace: "pre-wrap",
-              maxWidth: "750px",
-              textAlign: "center",
-              wordWrap: "break-word",
-              overflowWrap: "break-word",
+              position: 'absolute',
+              fontFamily: 'Inter',
+              fontSize: '48px',
+              fontWeight: '600',
+              letterSpacing: '-0.04em',
+              color: isLight ? 'black' : 'white',
+              top: '250px',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              whiteSpace: 'pre-wrap',
+              maxWidth: '750px',
+              textAlign: 'center',
+              wordWrap: 'break-word',
+              overflowWrap: 'break-word',
             }}
           >
             {title}
@@ -55,13 +55,13 @@ export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
         height: 441,
         fonts: [
           {
-            name: "Inter",
+            name: 'Inter',
             data: await interSemiBold,
-            style: "normal",
+            style: 'normal',
             weight: 400,
           },
         ],
-      },
+      }
     );
   } catch (e) {
     if (!(e instanceof Error)) throw e;
