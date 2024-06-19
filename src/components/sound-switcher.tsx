ecbeeb22825d4a7e0fec1fd3d48c35fc3f9c2922
jaem1n207 <tech.jmtt@gmail.com>
2024-06-20 00:36:24 +0900

    feat: 클라이언트에서만 렌더링되는 컴포넌트 구현

--- src/components/sound-switcher.tsx
+++ src/components/sound-switcher.tsx
@@ -4,6 +4,7 @@ import useSound from 'use-sound';
 
 import { SoundMax, SoundMute } from './icons';
 import { useSoundEnabled } from './model/sound';
+import { ClientGate } from './client-gate';
 
 export function SoundSwitcher() {
   const [isSoundEnabled, setIsSoundEnabled] = useSoundEnabled();
@@ -22,11 +23,13 @@ export function SoundSwitcher() {
       className="bd-relative bd-flex bd-size-full bd-items-center bd-justify-center"
       onClick={handleSoundToggle}
     >
-      {isSoundEnabled ? (
-        <SoundMax className="bd-size-1/2" />
-      ) : (
-        <SoundMute className="bd-size-1/2" />
-      )}
+      <ClientGate>
+        {isSoundEnabled ? (
+          <SoundMax className="bd-size-1/2" />
+        ) : (
+          <SoundMute className="bd-size-1/2" />
+        )}
+      </ClientGate>
       <span className="bd-sr-only">Toggle sound</span>
     </button>
   );
