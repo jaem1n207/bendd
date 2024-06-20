368c6a71616bddd2b9a56b051f6f43bc200ea4fb
jaem1n207 <tech.jmtt@gmail.com>
2024-06-21 01:57:23 +0900

    chore: lint-staged 파일 복구

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '**/*.ts?(x)': () => 'pnpm check-types',
+  '*.{md,mdx}': ['prettier --write'],
 };
