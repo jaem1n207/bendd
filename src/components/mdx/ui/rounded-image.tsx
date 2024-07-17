c6f5fe20f3cc4708469bab798448e91b962a419e
jaem1n207 <tech.jmtt@gmail.com>
2024-07-18 02:40:17 +0900

    refactor: mdx 컴포넌트 분리

---
+++ src/components/mdx/ui/rounded-image.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import { cn } from '@/lib/utils';
+
+type RoundedImageProps = Omit<
+  JSX.IntrinsicElements['img'],
+  'srcSet' | 'width' | 'height'
+>;
+
+export function RoundedImage({
+  className,
+  alt = '',
+  src,
+  ...props
+}: RoundedImageProps) {
+  if (!src) {
+    throw new Error('src is required for RoundedImage');
+  }
+
+  return (
+    <Image
+      alt={alt}
+      src={src}
+      width={1344}
+      height={768}
+      sizes="(max-width: 640px) 100vw, (max-width: 1280px) 50vw, 672px"
+      quality={80}
+      className={cn('bd-rounded-lg bd-object-cover', className)}
+      {...props}
+    />
+  );
+}
