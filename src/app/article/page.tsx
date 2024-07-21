0d5d8bc24b51207e4a9f4f13f05948780f095ecc
jaem1n207 <tech.jmtt@gmail.com>
2024-07-21 16:11:24 +0900

    chore: article 페이지 메타데이터에서 og 이미지 누락 수정

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,17 +1,25 @@
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 
 import { ArticleList } from '@/components/article';
 import { siteMetadata } from '@/lib/site-metadata';
 
-export const metadata: Metadata = {
-  title: '피드',
-  description: '작성한 글들을 모아놓은 공간입니다.',
-  openGraph: {
-    type: 'website',
-    url: `${siteMetadata.siteUrl}/article`,
-    siteName: 'Bendd의 피드',
-  },
-};
+export async function generateMetadata(
+  {},
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: '피드',
+    description: '작성한 글들을 모아놓은 공간입니다.',
+    openGraph: {
+      type: 'website',
+      url: `${siteMetadata.siteUrl}/article`,
+      siteName: 'Bendd의 피드',
+      images: [...previousImages],
+    },
+  };
+}
 
 export default function ArticlePage() {
   return (
