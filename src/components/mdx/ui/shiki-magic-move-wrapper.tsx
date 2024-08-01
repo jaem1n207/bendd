130e71ea68c76b71324f22e6ddc3c6e75d1e92e2
jaem1n207 <tech.jmtt@gmail.com>
2024-08-01 17:21:21 +0900

    refactor: shiki-magic-move 더 많은 코드 받을 수 있도록 인터페이스 개선

--- src/components/mdx/ui/shiki-magic-move-wrapper.tsx
+++ src/components/mdx/ui/shiki-magic-move-wrapper.tsx
@@ -1,129 +1,174 @@
-// TODO: Tabs 컴포넌트 구현하고 탭 변경 시 코드 변경 애니메이션 실행하도록 수정
-
 'use client';
 
-import { useEffect, useState } from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import type { HighlighterCore } from 'shiki';
 import { ShikiMagicMove } from 'shiki-magic-move/react';
+import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { cn } from '@/lib/utils';
 import { CopyToClipboard } from './copy-to-clipboard';
 
+import { Paragraph } from '@/components/ui/typography';
 import 'shiki-magic-move/dist/style.css';
 import '../style/magic-move.css';
+import { createMDXComponent } from './create-mdx-component';
 
-interface ShikiMagicMoveWrapperProps {
-  initCode: string;
-  nextCode: string;
-  lang: string;
-}
+type ShikiMagicMoveWrapperProps = z.infer<typeof ShikiMagicMoveWrapperSchema>;
 
-export function ShikiMagicMoveWrapper({
-  initCode,
-  nextCode,
+const ShikiMagicMoveWrapperSchema = z.object({
+  codeSnippets: z.array(
+    z.object({
+      code: z.string(),
+      description: z.string(),
+      title: z.string(),
+    })
+  ),
+  lang: z.string(),
+});
+
+function ShikiMagicMoveWrapper({
+  codeSnippets,
   lang,
 }: ShikiMagicMoveWrapperProps) {
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [code, setCode] = useState(initCode);
-  const [highlighter, setHighlighter] = useState<HighlighterCore | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const highlighter = useHighlighter();
+
+  const goToNext = useCallback(() => {
+    setCurrentIndex(prev => (prev + 1) % codeSnippets.length);
+  }, [codeSnippets.length]);
+
+  const goToPrevious = useCallback(() => {
+    setCurrentIndex(
+      prev => (prev - 1 + codeSnippets.length) % codeSnippets.length
+    );
+  }, [codeSnippets.length]);
+
+  const handleSelectChange = useCallback((value: string) => {
+    setCurrentIndex(parseInt(value, 10));
+  }, []);
+
+  const currentSnippet = useMemo(
+    () => codeSnippets[currentIndex],
+    [codeSnippets, currentIndex]
+  );
+
+  return (
+    <div className="bd-relative">
+      <div className="bd-mb-1 bd-flex bd-items-center bd-justify-between">
+        <Select
+          onValueChange={handleSelectChange}
+          value={currentIndex.toString()}
+        >
+          <SelectTrigger className="bd-mr-1 bd-flex-1">
+            <SelectValue placeholder="코드 예시 선택" />
+          </SelectTrigger>
+          <SelectContent>
+            {codeSnippets.map((snippet, index) => (
+              <SelectItem key={index} value={index.toString()}>
+                {snippet.title}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <div className="bd-flex bd-items-center bd-space-x-2">
+          <Button
+            size="icon"
+            variant="outline"
+            onClick={goToPrevious}
+            disabled={currentIndex === 0}
+          >
+            <ChevronLeft className="bd-size-4" />
+          </Button>
+          <span className="bd-text-sm">
+            {currentIndex + 1} / {codeSnippets.length}
+          </span>
+          <Button
+            size="icon"
+            variant="outline"
+            onClick={goToNext}
+            disabled={currentIndex === codeSnippets.length - 1}
+          >
+            <ChevronRight className="bd-size-4" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="bd-rounded-md bd-border bd-border-border bd-bg-background bd-px-4 bd-py-2 bd-shadow">
+        <Paragraph size="lg" className="bd-mb-2">
+          {currentSnippet.title}
+        </Paragraph>
+        <Paragraph>{currentSnippet.description}</Paragraph>
+      </div>
+
+      {highlighter && (
+        <div className="bd-relative">
+          <ShikiMagicMove
+            className={cn(
+              'bd-overflow-x-auto bd-rounded-lg bd-border bd-border-solid bd-border-border bd-bg-neutral-900 bd-px-0 bd-py-3 dark:bd-bg-gray-100',
+              'contrast-more:bd-border-current contrast-more:dark:bd-border-current'
+            )}
+            code={currentSnippet.code}
+            lang={lang}
+            theme="vitesse-dark"
+            highlighter={highlighter}
+            options={{
+              splitTokens: true,
+              duration: 750,
+              stagger: 3,
+              lineNumbers: true,
+            }}
+          />
+          <div className="bd-absolute bd-right-2 bd-top-2 bd-flex bd-gap-1 bd-opacity-0 bd-transition focus-within:bd-opacity-100 [div:hover>&]:bd-opacity-100">
+            <CopyToClipboard getValue={() => currentSnippet.code} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function useHighlighter(): HighlighterCore | undefined {
+  const [highlighter, setHighlighter] = useState<HighlighterCore>();
 
   useEffect(() => {
     async function initializeHighlighter() {
       const { createHighlighterCore } = await import('shiki');
       const getWasm = await import('shiki/wasm');
       const vitesseDark = await import('shiki/themes/vitesse-dark.mjs');
-      const highlighter = await createHighlighterCore({
+      const newHighlighter = await createHighlighterCore({
         themes: [vitesseDark],
         langs: [
           import('shiki/langs/typescript.mjs'),
-          /**
-           * 언어를 비동기 청크로 제공해 필요할 때 로드합니다.
-           */
-          () => import('shiki/langs/javascript.mjs'),
-          () => import('shiki/langs/html.mjs'),
-          () => import('shiki/langs/css.mjs'),
-          () => import('shiki/langs/json.mjs'),
-          () => import('shiki/langs/shell.mjs'),
-          () => import('shiki/langs/markdown.mjs'),
-          () => import('shiki/langs/yaml.mjs'),
-          () => import('shiki/langs/svelte.mjs'),
+          import('shiki/langs/javascript.mjs'),
+          import('shiki/langs/html.mjs'),
+          import('shiki/langs/css.mjs'),
+          import('shiki/langs/json.mjs'),
+          import('shiki/langs/shell.mjs'),
+          import('shiki/langs/markdown.mjs'),
+          import('shiki/langs/yaml.mjs'),
+          import('shiki/langs/svelte.mjs'),
         ],
         loadWasm: getWasm,
       });
-      setHighlighter(highlighter);
+      setHighlighter(newHighlighter);
     }
+
     initializeHighlighter();
   }, []);
 
-  const animate = () => {
-    setCode(nextCode);
-  };
-
-  const reset = () => {
-    setCode(initCode);
-  };
-
-  return (
-    <div className="magic-move-debug-style bd-relative">
-      {highlighter && (
-        <ShikiMagicMove
-          className={cn(
-            'bd-my-12 bd-overflow-x-auto bd-rounded-lg bd-border bd-border-solid bd-border-border bd-bg-neutral-900 bd-px-0 bd-py-3 dark:bd-bg-gray-100',
-            'contrast-more:bd-border-current contrast-more:dark:bd-border-current'
-          )}
-          onStart={() => {
-            setIsAnimating(true);
-          }}
-          onEnd={() => {
-            setIsAnimating(false);
-          }}
-          code={code}
-          lang={lang}
-          theme="vitesse-dark"
-          highlighter={highlighter}
-          options={{
-            splitTokens: true,
-            duration: 750,
-            stagger: 3,
-            lineNumbers: true,
-          }}
-        />
-      )}
-      <div className="bd-absolute -bd-top-8 bd-left-0 bd-m-2.5">
-        {code === nextCode ? (
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={reset}
-            title="Original code"
-            className="bd-group hover:[--active:1]"
-          >
-            원본 코드로 돌아가기
-          </Button>
-        ) : (
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={animate}
-            title="Changed code"
-            className="bd-group hover:[--active:1]"
-          >
-            변경된 코드 보기
-          </Button>
-        )}
-        <span
-          className={cn(
-            'bd-transition bd-text-green-500 bd-text-sm bd-ml-2',
-            isAnimating ? 'bd-animate-pulse' : 'bd-opacity-0'
-          )}
-        >
-          코드 변경 중...
-        </span>
-      </div>
-      <div className="bd-absolute bd-right-0 bd-top-0 bd-m-2.5 bd-flex bd-gap-1 bd-opacity-0 bd-transition focus-within:bd-opacity-100 [div:hover>&]:bd-opacity-100">
-        <CopyToClipboard getValue={() => code} />
-      </div>
-    </div>
-  );
+  return highlighter;
 }
+
+export const MDXShikiMagicMoveWrapper = createMDXComponent(
+  ShikiMagicMoveWrapper,
+  ShikiMagicMoveWrapperSchema
+);
