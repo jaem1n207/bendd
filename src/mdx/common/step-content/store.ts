264e55e5fefe0851ccba24adff8c9b43359e0afd
jaem1n207 <tech.jmtt@gmail.com>
2024-08-02 18:25:50 +0900

    refactor: 애니메이션 중 스텝 변경할 수 있도록 변경하고 스텝 변경 시 애니메이션 취소되도록 수정

--- src/mdx/common/step-content/store.ts
+++ src/mdx/common/step-content/store.ts
@@ -8,11 +8,6 @@ export type StepContentState = {
    * 다음으로 이동할 때는 1, 뒤로 이동할 때는 -1을 설정해 애니메이션 방향을 결정합니다.
    */
   direction: number;
-  /**
-   * 애니메이션 중인지 여부를 나타냅니다.
-   * 애니메이션 중엔 다음 단계로 넘어가거나 이전 단계로 돌아갈 수 없습니다.
-   */
-  isAnimating: boolean;
 };
 
 export type StepContentActions = {
@@ -20,7 +15,6 @@ export type StepContentActions = {
   setCurrentStep: (index: number) => void;
   nextStep: () => void;
   previousStep: () => void;
-  setIsAnimating: (isAnimating: boolean) => void;
 };
 
 export type StepContentStore = StepContentState & StepContentActions;
@@ -29,7 +23,6 @@ export const defaultInitState: StepContentState = {
   stepsData: [],
   currentStep: 0,
   direction: 0,
-  isAnimating: false,
 };
 
 export const createStepContentStore = (
@@ -39,27 +32,23 @@ export const createStepContentStore = (
     ...initState,
     setStepsData: stepsData => set({ stepsData, currentStep: 0 }),
     setCurrentStep: index => {
-      const { currentStep, isAnimating } = get();
-      if (isAnimating) return;
+      const { currentStep } = get();
       const direction = index > currentStep ? 1 : -1;
-      set({ currentStep: index, direction, isAnimating: true });
+      set({ currentStep: index, direction });
     },
     nextStep: () => {
-      const { currentStep, stepsData, isAnimating } = get();
-      if (isAnimating) return;
+      const { currentStep, stepsData } = get();
       const nextStep = Math.min(currentStep + 1, stepsData.length - 1);
       if (nextStep !== currentStep) {
-        set({ currentStep: nextStep, direction: 1, isAnimating: true });
+        set({ currentStep: nextStep, direction: 1 });
       }
     },
     previousStep: () => {
-      const { currentStep, isAnimating } = get();
-      if (isAnimating) return;
+      const { currentStep } = get();
       const previousStep = Math.max(currentStep - 1, 0);
       if (previousStep !== currentStep) {
-        set({ currentStep: previousStep, direction: -1, isAnimating: true });
+        set({ currentStep: previousStep, direction: -1 });
       }
     },
-    setIsAnimating: isAnimating => set({ isAnimating }),
   }));
 };
